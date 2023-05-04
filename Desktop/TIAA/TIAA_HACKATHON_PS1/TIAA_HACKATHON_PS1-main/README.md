4a42dd5941bab794aa77256bf6cf264de44167dd
Creator-1523 <charmi.git.23@gmail.com>
2023-05-04 09:08:06 +0530

    sidebar changes and add bus page and add driver page

---
+++ Desktop/TIAA/TIAA_HACKATHON_PS1/TIAA_HACKATHON_PS1-main/README.md
@@ -0,0 +1,1 @@
+A website to help users book bus services for inter-city travel as well as a portal for bus operators for adding bus information.
---
+++ Desktop/TIAA/TIAA_HACKATHON_PS1/TIAA_HACKATHON_PS1-main/src/components/Passenger/SearchBar.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import Datepicker from "react-tailwindcss-datepicker";
+import axios from "axios";
+
+
+const SearchBar = () => {
+    const [source, setSource] = useState("");
+    const [destination, setDestination] = useState("");
+    const [date, setDate] = useState({
+        startDate: null
+    });
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(source + " " + destination + " " + date.startDate);
+        // axios.post("http://localhost:4040/", { source, destination, date })
+        //     .then(response => {
+        //         console.log(response.data);
+        //     })
+        //     .catch(error => {
+        //         console.error(error);
+        //     });
+    };
+
+    return (
+        <div className="flex justify-center">
+            <form className="flex items-center mt-12 mx-auto" onSubmit={handleSubmit}>
+                <input
+                    className="block m-2 py-4 px-3 w-48 text-sm font-medium outline-none bg-transparent border border-gray-400 focus:border-green-500 rounded-lg"
+                    type="text"
+                    placeholder="Source"
+                    value={source}
+                    onChange={(e) => setSource(e.target.value)}
+                />
+                <img className="w-8 h-8" src="src\images\bidirectional-arrow.png"
+                    onClick={(e) => {
+                        var dest = destination;
+                        setDestination(source);
+                        setSource(dest)
+                    }} />
+                <input
+                    className="block m-2 py-4 px-3 w-48 text-sm font-medium outline-none bg-transparent border border-gray-400 focus:border-green-500 rounded-lg"
+                    type="text"
+                    placeholder="Destination"
+                    value={destination}
+                    onChange={(e) => setDestination(e.target.value)}
+                />
+                <div className="block m-2 py-1 px-3 w-72 outline-none bg-transparent border border-gray-400 rounded-lg">
+                    <Datepicker
+                        minDate={new Date()}
+                        useRange={false}
+                        popoverDirection="down"
+                        asSingle={true}
+                        value={date}
+                        onChange={(e) => setDate(e)}
+                    />
+                </div>
+                <button type="submit" className="btn btn-primary rounded-full ml-4 px-8 flex-shrink-0">
+                    Search
+                </button>
+            </form>
+        </div>
+    );
+}
+export default SearchBar;
